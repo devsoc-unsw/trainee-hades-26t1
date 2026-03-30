2f6b47c7bc836800ebe0064d689c302614ded0c2
Giselle <cryponiess@gmail.com>
2026-03-30 19:53:33 +1100

    backend and db setup

---
+++ backend/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string;
+
+// Initialize the Supabase client
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
