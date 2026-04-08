ddfd10dce928c4cbe9eec74c1d0424453b39b9e2
jleo0359 <jleo0359@uni.sydney.edu.au>
2026-04-08 21:14:06 +1000

    feat: add long break mode and auto-transition to short break on timer end + stylistic fixes on start/pause button #8

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,9 @@
+import PomodoroTimer from "@/components/PomodoroTimer";
+
 export default function Home() {
   return (
     <div className="">
+      <PomodoroTimer />
       <main className="font-(family-name:--font-pixelify) ">Hello world</main>
     </div>
   );
