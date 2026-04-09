110fc773c3c45e0e0cd3f04092d1c1746769b0c5
Nathaniel Tan <nathanclw53@gmail.com>
2026-04-09 23:04:50 +1000

    supabase setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js"
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// will set up supabase later
+const supabase = () => createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+)
 
 // test router
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+//   console.log("Response sent");
+// });
+
+// supabase test
+app.get("/test", async (req, res) => {
+  const { data, error } = await supabase().from("users").select("*").limit(1)
+
+  if (error) {
+    console.log(error)
+    return res.status(500).json({error: error.message})
+  }
+
+  res.json({
+    message: "Supabase connected successfully",
+    data
+  })
 });
 
 app.listen(PORT, () => {
