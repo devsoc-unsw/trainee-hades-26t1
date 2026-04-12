45c02ca7a86f5dc09461e20cd66a915bc2744778
Pua Yee Ming <yeemingpua@gmail.com>
2026-04-12 13:12:57 +1000

    fix: fixed tooltip border issue

--- frontend/src/components/RoomCard.tsx
+++ frontend/src/components/RoomCard.tsx
@@ -11,57 +11,66 @@ type RoomCardProps = {
   imageUrl?: string;
 };
 
-export default function RoomCard({ id, name, location, imageUrl }: RoomCardProps) {
-  const [tooltip, setToolTip] = useState< {x: number, y: number} | null>(null);
+export default function RoomCard({
+  id,
+  name,
+  location,
+  imageUrl,
+}: RoomCardProps) {
+  const [tooltip, setToolTip] = useState<{ x: number; y: number } | null>(null);
 
   return (
-    <div className="relative" onMouseMove={(e) => {
-        {/*Keep location bar in bound*/}
+    <div
+      className="relative"
+      onMouseMove={(e) => {
+        {
+          /*Keep location bar in bound*/
+        }
         const mouse = e.currentTarget.getBoundingClientRect();
         setToolTip({
-            x: e.clientX - mouse.left,
-            y: e.clientY - mouse.top,
-        })
-    }}
-    onMouseLeave={() => setToolTip(null)}
+          x: e.clientX - mouse.left,
+          y: e.clientY - mouse.top,
+        });
+      }}
+      onMouseLeave={() => setToolTip(null)}
     >
-    <Link href={`/room/${id}`}>
+      <Link href={`/room/${id}`}>
         <div className="relative rounded-2xl overflow-hidden transition-transform duration-200 hover:scale-[1.02] border-2 border-(--dark-blue) h-48">
-        {/* Background, image || white */}
-        {imageUrl ? (
-            <Image
-            src={imageUrl}
-            alt={name}
-            fill
-            className="object-cover"
-            />
-        ) : (
+          {/* Background, image || white */}
+          {imageUrl ? (
+            <Image src={imageUrl} alt={name} fill className="object-cover" />
+          ) : (
             <div className="w-full h-full bg-white" />
-        )}
+          )}
 
-        {/* Class name */}
-        <div className="absolute bottom-0 left-0 right-0 p-4">
+          {/* Class name */}
+          <div className="absolute bottom-0 left-0 right-0 p-4">
             <span
-            className={`text-xs font-(family-name:--font-pixelify) ${
+              className={`text-xs font-(family-name:--font-pixelify) ${
                 imageUrl ? "text-white" : "text-(--dark-blue)"
-            }`}
+              }`}
             >
-            {name}
+              {name}
             </span>
+          </div>
         </div>
-        </div>
-    </Link>
+      </Link>
 
-    {/* Mouse Tooltip*/}
-    {tooltip && (
-        <div className="pointer-events-none absolute z-50 flex items-center gap-2 px-3 py-2 rounded-lg bg-(--pastel-yellow) border-2 border-(--dark-blue)"
-        style={{ left: tooltip.x, top: tooltip.y - 45}} >
-        <MapPin size={15} className="tex-(--dark-blue)" />
-        <span className="text-[0.75rem] font-(family-name:--font-pixelify) text-(--dark-blue) uppercase">
+      {/* Mouse Tooltip*/}
+      {tooltip && (
+        <div
+          className="pointer-events-none absolute z-50 flex items-center gap-2 px-3 py-2 rounded-lg bg-(--pastel-yellow) border-2 border-(--dark-blue) whitespace-nowrap"
+          style={{
+            left: tooltip.x,
+            top: tooltip.y - 45,
+          }}
+        >
+          <MapPin size={15} className="text-(--dark-blue)" />
+          <span className="text-[0.75rem] font-(family-name:--font-pixelify) text-(--dark-blue) uppercase">
             {location}
-        </span>
+          </span>
         </div>
-    )}
+      )}
     </div>
   );
 }
