97243bc93454c553250085a9384efcb1997b0acd
Nathaniel Tan <nathanclw53@gmail.com>
2026-04-12 13:52:43 +1000

    tested supabase codebase initial

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,9 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const supabase = () => createClient(
+app.use(express.json());
+
+const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_ANON_KEY!
 )
@@ -20,7 +22,7 @@ const supabase = () => createClient(
 
 // supabase test
 app.get("/test", async (req, res) => {
-  const { data, error } = await supabase().from("users").select("*").limit(1)
+  const { data, error } = await supabase.from("users").select("*").limit(1)
 
   if (error) {
     console.log(error)
@@ -31,6 +33,46 @@ app.get("/test", async (req, res) => {
     message: "Supabase connected successfully",
     data
   })
+});
+
+// gets the user
+// const { data: { users } } = await supabase().auth.getUser()
+
+// logout handler (temporary)
+// await supabase().auth.signOut()
+
+// register
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.json(data);
+});
+
+// signin
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  // include an error check for non-existant credentials
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.json(data);
 });
 
 app.listen(PORT, () => {
