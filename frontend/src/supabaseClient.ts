0d38b14e7d9ffcbab35b2025021467c805e8909e
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2026-05-10 23:59:14 +1000

    handle auth for creating room

---
+++ frontend/src/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Grab the environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+// Create and export the client
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
