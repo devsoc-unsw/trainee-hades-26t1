c5f75db792ef88e2890e503691986cae97ee61e3
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2026-05-18 23:07:50 +1000

    feat: integrate profile apis with FE

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,82 +1,150 @@
 "use client";
 import Navbar from "@/components/Navbar";
-import FilterBar from "@/components/FilterBar";
-import RoomCard from "@/components/RoomCard";
+import Loading from "@/components/Loading";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pencil } from "lucide-react";
-
-//Mocked Data
-const unlockedRooms = [
-  {
-    id: 1,
-    name: "Data Struct",
-    location: "Ainsworth Building",
-    imageUrl: "/ainsworth-building.jpg",
-  },
-  { id: 2, name: "Geography", location: "Block C, Room 305", imageUrl: "" },
-  { id: 3, name: "Geography", location: "Block C, Room 305", imageUrl: "" },
-  { id: 4, name: "Geography", location: "Block C, Room 305", imageUrl: "" },
-];
-
-//Mocked Data
-const currency = 4200;
+import { supabase } from "@/supabaseClient";
 
 export default function Profile() {
   const [editing, setEditing] = useState(false);
   const [userName, setUserName] = useState("User");
-  const [filter, setFilter] = useState("");
+  const [currency, setCurrency] = useState(0);
+  const [email, setEmail] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
 
-  const filteredRooms = unlockedRooms.filter((room) =>
-    room.name.toLowerCase().includes(filter.toLowerCase()),
-  );
+  const getUserData = async () => {
+    try {
+      const { data: { session } } = await supabase.auth.getSession();
+      console.log(session);
+      const token = session?.access_token;
+      if (!token) {
+        console.error("No access token found");
+        setLoading(false);
+        return;
+      }
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/profile`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await res.json();
+      setUserName(data.name);
+      setAvatarUrl(data.avatar_url);
+      setCurrency(data.currency);
+      setEmail(session?.user.email || null);
+      setLoading(false);
+    } catch (err) {
+      console.error("Error fetching user data:", err);
+      setLoading(false);
+    }
+  }
+
+  const updateUserData = async (updatedFields: { name?: string }) => {
+    try {
+      const { data: { session } } = await supabase.auth.getSession();
+      const token = session?.access_token;
+      if (!token) {
+        console.error("No access token found");
+        return;
+      }
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/profile`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(updatedFields),
+      });
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        console.error("Error updating user data:", errorData);
+      } else {
+        getUserData(); // Refresh data after update
+      }
+    } catch (err) {
+      console.error("Error updating user data:", err);
+    }
+  };
+
+  const handleNameChange = (newName: string) => {
+    // On key down
+    updateUserData({ name: newName });
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
     <div className="pt-18 min-h-screen bg-(--light-blue)">
       <Navbar />
       <main className="flex flex-col items-center px-10 py-33 gap-8">
-        {/* Profile */}
-        <div className="relative w-full max-w-2xl bg-(--dark-blue) rounded-3xl px-10 pt-20 sm:pt-35 pb-8 flex flex-col items-center">
-          {/* Profile Pic */}
-          <div className="absolute -top-16 sm:-top-29 w-36 h-36 sm:w-60 sm:h-60 rounded-full bg-(--pastel-yellow) border-4 border-(--dark-blue) overflow-hidden">
-            <Image
-              src="/ainsworth-building.jpg" //hardcoded version
-              alt="👽"
-              fill
-              className="object-cover"
-            />
-          </div>
-          {/* Currency */}
-          <p className="absolute top-4 left-6 text-white text-sm sm:text-xl font-(family-name:--font-pixelify)">
-            Currency: {currency}
-          </p>
-          {/* Name */}
-          <div className="flex items-center gap-2">
-            {editing ? (
-              <input
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && setEditing(false)}
-                className="bg-transparent border-b-2 border-white text-white text-2xl font-(family-name:--font-pixelify) tracking-widest text-center outline-none"
-              />
-            ) : (
-              <h1 className="text-white text-base sm:text-xl font-(family-name:--font-pixelify) tracking-widest">
-                Username: {userName}
-              </h1>
-            )}
-            {/* Edit name tool */}
-            <Pencil
-              size={18}
-              className="text-white/70 cursor-pointer hover:text-white"
-              onClick={() => setEditing(true)}
-            />
-          </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            {/* Profile */}
+            <div className="relative w-full max-w-2xl bg-(--dark-blue) rounded-3xl px-10 pt-20 sm:pt-35 pb-8 flex flex-col items-center">
+              {/* Profile Pic */}
+              <div className="absolute -top-16 sm:-top-29 w-36 h-36 sm:w-60 sm:h-60 rounded-full bg-(--pastel-yellow) border-4 border-(--dark-blue) overflow-hidden">
+                {avatarUrl ? (
+                  <Image
+                    src={avatarUrl}
+                    alt="profile picture"
+                    fill
+                    className="object-cover"
+                  />
+                ) : (
+                  <div className="w-full h-full bg-(--pastel-yellow) flex items-center justify-center">
+                    <span className="text-(--dark-blue) text-4xl">👽</span>
+                  </div>
+                )}
+              </div>
+              {/* Currency */}
+              <p className="absolute top-4 left-6 text-white text-sm sm:text-xl font-(family-name:--font-pixelify)">
+                Currency: {currency}
+              </p>
+              {/* Name */}
+              <div className="flex items-center gap-2">
+                {editing ? (
+                  <input
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleNameChange(userName);
+                        setEditing(false);
+                      }
+                    }}
+                    className="bg-transparent border-b-2 border-white text-white text-2xl font-(family-name:--font-pixelify) tracking-widest text-center outline-none"
+                  />
+                ) : (
+                  <h1 className="text-white text-base sm:text-xl font-(family-name:--font-pixelify) tracking-widest">
+                    Username: {userName}
+                  </h1>
+                )}
+                {/* Edit name tool */}
+                <Pencil
+                  size={18}
+                  className="text-white/70 cursor-pointer hover:text-white"
+                  onClick={() => setEditing(true)}
+                />
+              </div>
 
-          {/* Email */}
-          <p className="text-white text-base sm:text-xl font-(family-name:--font-pixelify) mt-2">
-            email: example@gmail.com {/* Hardcoded version */}
-          </p>
-        </div>
+              {/* Email */}
+              <p className="text-white text-base sm:text-xl font-(family-name:--font-pixelify) mt-2">
+                email: {email}
+              </p>
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
